952824c741fa5e69d643725ec9301c4d15ad306a
ornvilasinee <eyeornvilasinee@gmail.com>
2024-10-08 13:31:04 +0700

    Add files via upload

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+a = float(input("Enter the first number: \n"))
+y = float(input("Enter the second number: \n"))
+print( a, "x", y, "=", a*y)
+x = float(a*y)
+if x == 0 :
+    print("This number is both positive and negative")
+elif x < 0 :
+    print("This number is negative.")
+else :
+    print("This number is positive.")
