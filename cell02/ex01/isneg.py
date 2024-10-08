c8fdf3d7e813ecb07b8b7b3bb5741a669c57a4e3
ornvilasinee <eyeornvilasinee@gmail.com>
2024-10-08 12:59:10 +0700

    Add files via upload

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+x = float(input())
+if x == 0 :
+    print("This number is both positive and negative")
+elif x < 0 :
+    print("This number is negative.")
+else :
+    print("This number is positive.")
